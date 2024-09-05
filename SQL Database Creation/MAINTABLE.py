26d0a688f280891339526cae2a49722ec9e5a594
JoelJuby <115355861+JoelJuby@users.noreply.github.com>
2024-09-05 12:23:07 +0530

    Update MAINTABLE.py

--- SQL Database Creation/MAINTABLE.py
+++ SQL Database Creation/MAINTABLE.py
@@ -1,6 +1,6 @@
 import mysql.connector
 # create table 'MAINTABLE'
-mydb=mysql.connector.connect(host='localhost',user='root',password='joel2003',database='TRAVEL_AND_TOURISM')
+mydb=mysql.connector.connect(host='localhost',user='root',password='*****',database='TRAVEL_AND_TOURISM')
 mycursor=mydb.cursor( )
 a="create table MAINTABLE(COUNTRY varchar(20),CONTINENT varchar(20),DESCRIPTION varchar (2000))"
 mycursor.execute(a)
