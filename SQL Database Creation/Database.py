b557ff8439ee6f364e2078fcd2f8e24ce8726783
JoelJuby <115355861+JoelJuby@users.noreply.github.com>
2024-09-05 12:21:21 +0530

    Update Database.py

--- SQL Database Creation/Database.py
+++ SQL Database Creation/Database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 #create 'database travel_and_tourism'
-mydb=mysql.connector.connect(host='localhost',user='root',password='joel2003',)
+mydb=mysql.connector.connect(host='localhost',user='root',password='*****',)
 mycursor=mydb.cursor( )
 mycursor.execute("create database TRAVEL_AND_TOURISM")
 mydb.commit( )
