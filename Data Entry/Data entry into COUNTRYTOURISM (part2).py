43c1ca724d1a31bf1ad57935ee067f1dd597b4ff
JoelJuby <115355861+JoelJuby@users.noreply.github.com>
2024-09-05 12:18:50 +0530

    Update Data entry into COUNTRYTOURISM (part2).py

--- Data Entry/Data entry into COUNTRYTOURISM (part2).py
+++ Data Entry/Data entry into COUNTRYTOURISM (part2).py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="joel2003",database="TRAVEL_AND_TOURISM")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="*****",database="TRAVEL_AND_TOURISM")
 mycursor=mydb.cursor()
 
 A1="insert into countrytourism values('France','Eiffel Tower,Paris');"
