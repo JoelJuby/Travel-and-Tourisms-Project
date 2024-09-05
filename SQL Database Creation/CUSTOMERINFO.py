e44dfb8a2a4a3c73371178bf74ce3a9086212379
JoelJuby <115355861+JoelJuby@users.noreply.github.com>
2024-09-05 12:21:07 +0530

    Update CUSTOMERINFO.py

--- SQL Database Creation/CUSTOMERINFO.py
+++ SQL Database Creation/CUSTOMERINFO.py
@@ -1,6 +1,6 @@
 import mysql.connector
 # create table 'CUSTOMERINFO'
-mydb=mysql.connector.connect(host='localhost',user='root',password='joel2003',database='TRAVEL_AND_TOURISM')
+mydb=mysql.connector.connect(host='localhost',user='root',password='*****',database='TRAVEL_AND_TOURISM')
 mycursor=mydb.cursor( )
 f="create table CUSTOMERINFO(MOBILE varchar(20),EMAIL varchar(60),ID varchar (8),NAME varchar(30),STATUS varchar(15))"
 mycursor.execute(f)
