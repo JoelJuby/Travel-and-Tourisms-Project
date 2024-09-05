b8699a5eee9e21b563f7b84a3674b8c4b046b3aa
JoelJuby <115355861+JoelJuby@users.noreply.github.com>
2024-09-05 12:20:40 +0530

    Update CONVERSIONTABLE.py

--- SQL Database Creation/CONVERSIONTABLE.py
+++ SQL Database Creation/CONVERSIONTABLE.py
@@ -1,6 +1,6 @@
 import mysql.connector
 # create table 'CONVERSION'
-mydb=mysql.connector.connect(host='localhost',user='root',password='joel2003',database='TRAVEL_AND_TOURISM')
+mydb=mysql.connector.connect(host='localhost',user='root',password='*****',database='TRAVEL_AND_TOURISM')
 mycursor=mydb.cursor( )
 e="create table CONVERSION(COUNTRY varchar(20),RATEBHD decimal(5,2),RATEUSD decimal(5,2),RATEINR decimal(5,2))"
 mycursor.execute(e)
